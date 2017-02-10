74445e8bd167949c378c6c440e4cb5b409c7b5e8
Boris <bmusa@localvox.com>
2017-02-10 17:13:52 -0500

    Updated docs copyright

--- docs/conf.py
+++ docs/conf.py
@@ -55,8 +55,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Vivial Connect'
-copyright = u'2017, Boris Musa'
-author = u'Boris Musa'
+copyright = u'2017, Vivial Connect'
+author = u'Vivial Connect'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
